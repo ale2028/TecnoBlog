d9a61c17c55136d8f6cf0be1c1317f64ba1163e6
Alejandra Alvarez <Alejandra Alvarez>
2018-06-14 22:26:37 -0600

    Agregar archivos de proyecto.

---
+++ TecnoBlog.Frontend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TecnoBlog.Frontend.Startup))]
+namespace TecnoBlog.Frontend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
